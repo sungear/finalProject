63518e122654de157d3b22ad9f843924f3d0e29a
Sunny <sk.singh@hotmail.be>
2016-12-12 16:41:45 +0100

    dice throwing depend on applied pressure

--- finalProject/Assets/CustomAssets/Scenes/test/ClickTime.cs
+++ finalProject/Assets/CustomAssets/Scenes/test/ClickTime.cs
@@ -3,23 +3,32 @@ using System.Collections;
 
 public class ClickTime : MonoBehaviour {
     public float temps = 0;
-    public Vector2 initPos;
+    public Vector3 initP;
     public bool init;
-    public Vector2 finalPos;
+    public Vector3 finalP;
     public bool final;
     public GameObject dice;
+    public float directionX;
+    public float directionY;
+    public string direction;
+
+
+    public Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
 	   init = false;
        final = false;
+        rb = dice.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
             if ( Input.GetMouseButtonDown (0) )
         {
                temps = Time.time ;
+               initP = Input.mousePosition;
+               // dice.transform.position = new Vector3(initP.x, initP.y, dice.transform.position.z);               
         }
      
         if ( Input.GetMouseButtonUp(0) && (Time.time - temps) < 0.2 )
@@ -31,33 +40,36 @@ public class ClickTime : MonoBehaviour {
        {
             print("long");
        }
+       if (Input.GetMouseButtonUp(0)) {
+            finalP = Input.mousePosition;
+            directionX = finalP.x - initP.x;
+            directionY = finalP.y - initP.y;  
+
+       if (directionX != 0 && directionY != 0) {
+            if (Mathf.Abs(directionX) > Mathf.Abs(directionY)) {
+                if (directionX > 0) {
+                    print("right");
+                    rb.velocity = new Vector3(temps, 0, 0);
+                }
+                else {
+                    print("left");
+                    rb.velocity = new Vector3(-temps, 0, 0);
+                }
+            }
+            else {
+                if (directionY > 0) {
+                    print("up");
+                    rb.velocity = new Vector3(0, 0, temps);
+                }
+                else {
+                    print("down");
+                    rb.velocity = new Vector3(0, 0, -temps);
+                }
+            }
+       } 
+
+       temps = 0;
+       }
 	
 	}
-    void OnGUI()
-    {    if ( Input.GetMouseButtonDown (0) )
-        {
-               if (!init) {              
-
-                    Event e = Event.current;
-                    initPos = e.mousePosition;
-
-                    dice.transform.position = new Vector3(initPos.x, initPos.y, dice.transform.position.z);
-                    init = true;
-               }
-        }
-
-
-        if (Input.GetMouseButtonUp(0)) {
- 
-
-            Event e = Event.current;
-            if (e.isMouse) {
-                Vector2 mouseXY = new Vector2 (e.mousePosition.x - Screen.width/2, e.mousePosition.y - Screen.height/2);
-               if (!final) {             
-                    finalPos = mouseXY;
-                    final = true;
-               }
-            }
-        }
-    }
 }
