20676136a655b1479a59a739fabf3147c8f77ce0
Sunny <sk.singh@hotmail.be>
2016-12-15 16:49:13 +0100

    rotation and slerp

--- finalProject/Assets/CustomAssets/Scenes/test/ClickTime.cs
+++ finalProject/Assets/CustomAssets/Scenes/test/ClickTime.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ClickTime : MonoBehaviour {
+public class ClickTime : MonoBehaviour
+{
     public float temps = 0;
     public Vector3 initP;
     public bool init;
@@ -11,65 +12,82 @@ public class ClickTime : MonoBehaviour {
     public float directionX;
     public float directionY;
     public string direction;
+    public bool clicked;
 
 
     public Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
-	   init = false;
-       final = false;
+    // Use this for initialization
+    void Start()
+    {
+        init = false;
+        final = false;
+        clicked = false;
         rb = dice.GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-            if ( Input.GetMouseButtonDown (0) )
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-               temps = Time.time ;
-               initP = Input.mousePosition;
-               // dice.transform.position = new Vector3(initP.x, initP.y, dice.transform.position.z);               
+            clicked = true;
+            initP = Input.mousePosition;
+            // dice.transform.position = new Vector3(initP.x, initP.y, dice.transform.position.z);               
         }
-     
-        if ( Input.GetMouseButtonUp(0) && (Time.time - temps) < 0.2 )
-       {
+
+        if (Input.GetMouseButtonUp(0) && (Time.time - temps) < 0.2)
+        {
             print("short");
-       }
-     
-        if ( Input.GetMouseButtonUp(0) && (Time.time - temps) > 0.2 )
-       {
+        }
+
+        if (Input.GetMouseButtonUp(0) && (Time.time - temps) > 0.2)
+        {
             print("long");
-       }
-       if (Input.GetMouseButtonUp(0)) {
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            clicked = false;
             finalP = Input.mousePosition;
             directionX = finalP.x - initP.x;
-            directionY = finalP.y - initP.y;  
+            directionY = finalP.y - initP.y;
 
-       if (directionX != 0 && directionY != 0) {
-            if (Mathf.Abs(directionX) > Mathf.Abs(directionY)) {
-                if (directionX > 0) {
-                    print("right");
-                    rb.velocity = new Vector3(temps, 0, 0);
+            if (directionX != 0 && directionY != 0)
+            {
+                if (Mathf.Abs(directionX) > Mathf.Abs(directionY))
+                {
+                    if (directionX > 0)
+                    {
+                        print("right");
+                        rb.velocity = new Vector3(temps*5, 0, 0);
+                    }
+                    else {
+                        print("left");
+                        rb.velocity = new Vector3(-temps*5, 0, 0);
+                    }
                 }
                 else {
-                    print("left");
-                    rb.velocity = new Vector3(-temps, 0, 0);
+                    if (directionY > 0)
+                    {
+                        print("up");
+                        rb.velocity = new Vector3(0, temps * 5, 0);
+                    }
+                    else {
+                        print("down");
+                        rb.velocity = new Vector3(0, -temps * 5, 0);
+                    }
                 }
             }
-            else {
-                if (directionY > 0) {
-                    print("up");
-                    rb.velocity = new Vector3(0, 0, temps);
-                }
-                else {
-                    print("down");
-                    rb.velocity = new Vector3(0, 0, -temps);
-                }
-            }
-       } 
 
-       temps = 0;
-       }
-	
-	}
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            temps += Time.deltaTime;
+        }
+        else
+        {
+            temps = 0;
+        }
+    }
 }
--- finalProject/Assets/CustomAssets/Scripts/Rotation.cs
+++ finalProject/Assets/CustomAssets/Scripts/Rotation.cs
@@ -9,9 +9,11 @@ public class Rotation : MonoBehaviour {
     float rotationSpeed;
     bool begin;
     bool turning;
+    float rotationBonus;
+    public float pressTimer = 0;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
     }
 	
 	// Update is called once per frame
@@ -22,25 +24,40 @@ public class Rotation : MonoBehaviour {
         if (GameObject.Find("StartManager").GetComponent<FinishRound>().diceTurn >= 0){   
             if (!dice.GetComponent<Dice>().turning) {
 
+                if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow) ||
+                    Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+                {
+                    pressTimer += Time.deltaTime;
+                }
+
                 if (Input.GetKeyUp(KeyCode.RightArrow)) {
                     ChangeAtRotation(dice, 0, 0, 90);
+                    rotationBonus = 0.1f;
                 }
 
                 else if (Input.GetKeyUp(KeyCode.LeftArrow)) {
                     ChangeAtRotation(dice, 0, 0, -90);
+                    rotationBonus = 0.1f;
                 }
 
                 else if (Input.GetKeyUp(KeyCode.UpArrow)) {
                     ChangeAtRotation(dice, -90, 0, 0);
+                    rotationBonus = 0.1f;
                 }
 
                 else if (Input.GetKeyUp(KeyCode.DownArrow)) {
                     ChangeAtRotation(dice, 90, 0, 0);
+                    rotationBonus = 0.1f;
                 }  
+            }
+            else
+            {
+                pressTimer = 0;
             }
 
             rotationSpeed += Time.deltaTime*10.0f;
-            dice.rotation = Quaternion.Slerp(dice.GetComponent<Dice>().startRot, dice.GetComponent<Dice>().endRot, rotationSpeed * 0.1f);
+            dice.rotation = Quaternion.Slerp(dice.GetComponent<Dice>().startRot, dice.GetComponent<Dice>().endRot, rotationSpeed * rotationBonus);
+            rotationBonus += 0.01f;
             if (Quaternion.Angle(dice.GetComponent<Dice>().endRot, dice.rotation) <= 1.0){
                 dice.GetComponent<Dice>().turning = false;
                 if (GameObject.Find("StartManager").GetComponent<FinishRound>().diceTurn <= 0 &&
@@ -64,7 +81,7 @@ public class Rotation : MonoBehaviour {
         GameObject.Find("StartManager").GetComponent<StartManager>().selectedDice.transform.Find("MyDice00").GetComponent<Dice>().side2 = null;
 
         dice.GetComponent<Dice>().startRot = dice.rotation;
-        dice.Rotate(x, y, z, Space.World);
+        dice.Rotate(x, y, z /** pressTimer * 7*/, Space.World);
         dice.GetComponent<Dice>().endRot = dice.rotation;
 
         rotationSpeed = 0;
